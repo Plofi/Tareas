c1a9e8760e19a52e752c5404ebd31cd310aca69f
Plofi <69993131+Plofi@users.noreply.github.com>
2020-09-10 21:14:24 -0300

    Update AyP_Unidad 3_Ej 3.1.b_Def Calculo_hms.py

--- Clase 4/Algoritmos y Programación - Unidad 3/AyP_Unidad 3_Ej 3.1.b_Def Calculo_hms.py
+++ Clase 4/Algoritmos y Programación - Unidad 3/AyP_Unidad 3_Ej 3.1.b_Def Calculo_hms.py
@@ -1,21 +1,18 @@
 # Ej. 3.1.b. Escribir una función que calcule la duración en hs, min y seg de un intervalo dado en segundos:
 #En programa sería:
 
-def calculo_hms (s):
+def calculo_hms(segundos):
     h = 0
     m = 0
-    while s>=3600:
-        hora = s-3600
-        s = hora
+    while segundos>=3600:
+        hora = segundos -3600
+        segundos = hora
         h = h+1
-    else:    
-        while s>=60:
-            minutos = s-60
-            s = minutos
+       
+        while segundos>=60:
+            minutos = segundos-60
+            segundos = minutos
             m = m+1
-    s = int(s)
-    return((print("Su intervalo de", segundos, "segundos, expresado en horas,minutos y segundos es: ",h,":", m,":",s)))
+        return (h,":", m,":",segundos)
 
-s = int(input("Ingrese el intervalo en segundos, le diremos cuantas hs,min y seg son:"))
-segundos = s
-print(calculo_hms(s))
+calculo_hms(segundos)
